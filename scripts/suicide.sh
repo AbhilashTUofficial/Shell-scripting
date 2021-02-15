66f95c730abbd32acdcece7d60534c1b4b9e1dc5
AbhilashTUofficial <abhilashstorm@gmail.com>
2021-02-15 18:41:48 +0530

    dead

--- scripts/suicide.sh
+++ scripts/suicide.sh
@@ -6,4 +6,4 @@ echo "“I can’t see any way out,”—no matter how casually or"
 echo "jokingly said—may indicate serious suicidal feelings."
 echo "*************************************************************************"
 echo "dont end up like this file"
-
+rm suicide.sh
